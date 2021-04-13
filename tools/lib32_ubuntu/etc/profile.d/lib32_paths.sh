784a3cf7706e40a9f5827d78d64eda1c5ecc92d5
Rufas Wan <rufaswan@gmail.com>
2021-04-13 10:23:54 +0800

    [lib32] pcsx2

---
+++ tools/lib32_ubuntu/etc/profile.d/lib32_paths.sh
@@ -0,0 +1,4 @@
+# add 32-bit library to search path
+export LD_LIBRARY_PATH=/lib32:"$LD_LIBRARY_PATH"
+export GTK_PATH=/lib32/gtk-2.0:"$GTK_PATH"
+export LIBGL_DRIVERS_PATH=/lib32/dri:"LIBGL_DRIVERS_PATH"
